0eb4f65e18590e84e9e0ad57ffb1a5bc2366c419
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2016-09-12 22:36:42 +0200

    Added add to cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+        session[:counter] = 1
+    else
+        session[:counter] += 1
+    end
+
+    @views = session[:counter]
   end
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -7,7 +7,12 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_select '#columns #side a', minimum: 4
     assert_select '#main .entry', 3
     assert_select 'h3', 'Programming Ruby 2.3'
-    assert_select '.price', /\$[,\d]+\.\d\d/
+    assert_select '.price', /PLN [,\d]+\.\d\d/
   end
 
+  test "should see the counter after 6 visits" do
+    6.times { get store_index_url }
+
+    assert_select '#counter', 'Hey, you have viewed our catalog 6 times. Do you need any help?'
+  end
 end
