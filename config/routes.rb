97341878ca322901c99a89f170250aa6205e704e
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2016-09-22 23:37:27 +0200

    Iteration G: Checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store'
+
   get 'store/index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  root 'store#index', as: 'store'
 end
